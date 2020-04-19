5d56438daf8190fd7141ae25e2176f89b0f197cf
TottiPuc <dayan3846@gmail.com>
2020-04-19 18:05:15 -0500

    creando el algorimto de kahn para organizar los nodos

--- MLP.py
+++ MLP.py
@@ -39,6 +39,57 @@ class Input(Neuron):
                 
 #subclase de Neuron para realizar calculos                
 class Add(Neuron):
-    def __init__(self,x,y):
-        Neuron.__init__(self,[x,y])
+    def __init__(self,*inputs):
+        Neuron.__init__(self,inputs)
+        
+    def forward(self):
+        x_value = self.nodos_entrada[0].valor
+        y_value = self.nodos_entrada[1].valor
+        self.valor = x_value + y_value
+        
+def topologia_sort(feed_dict):
+    """
+    Algoritmo de kahn para clasificar los nodos en orden topologico 
+    
+    Parameters
+    ----------
+    feed_dict : diccionario
+        diccionario donde la clave es un nodo de entrada y el valor es el respectico feed de valor para ese nodo
+
+    Returns: una lista de nodos ordenados L
+    -------
+    """ 
+    
+    input_nodos = [n for n in feed_dict.keys()]
+    
+    G = {}
+    nodos = [n for n in input_nodos]
+    while len(nodos) > 0:
+        n = nodos.pop(0)
+        if n not in G:
+            G[n] = {'in':set(), 'out':set()}
+        for m in n.nodos_salida:
+            if m not in G:
+               G[m] = {'in':set(), 'out':set()} 
+            G[n]['out'].add(m)
+            G[m]['in'].add(n)
+            nodos.append(m)
+    
+    L = {}
+    S = set(input_nodos)
+    while len(S) > 0:
+        n = S.pop()
+        
+        if isinstance(n,Input):
+            n.valor = feed_dict[n]
+            
+        L.append(n)
+        for m in n.nodos_salida:
+            G[n]['out'].remove(m)
+            G[m]['in'].remove(n)
+            if len(G[m]['in']) == 0:
+                S.add(m)
+    return L
+    
+    
     
