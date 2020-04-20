186a7694bdc044a7109354925e36c7179b16ac85
TottiPuc <dayan3846@gmail.com>
2020-04-19 23:48:12 -0500

    version final pruebas con dataset

--- MLP.py
+++ MLP.py
@@ -10,16 +10,18 @@ class Neuron(object):
         
         self.nodos_entrada = nodos_entrada      # nodos que reciben las entradas
         self.nodos_salida = []                  # nodos para los cuales se pasan los valores
-        self.valor = None                       # con el fin de actualizar al momento de retropropagar
+        self.valor = None                       # el valor calculado de este nodo es definido ejecutando el metodo forward
         self.gradientes = {}                    # almacenara las derivadas que seran usadas para actualizar los pesos
         #para cada nodod de entrada sera adicionado como un nodo de salida
         for n in self.nodos_entrada:
             n.nodos_salida.append(self)
 
     def forward(self):
+        #todo nodo que use esta clase base, necesita definir su propio metodo forward
         raise NotImplementedError
 
     def backward(self):
+        #todo nodo que use esta clase base, necesita definir su propio metodo backward
         raise NotImplementedError
     
     
@@ -32,15 +34,20 @@ class Input(Neuron):
         #un nodo de entrada no posee neurones de entrada
         #por esta razon no es necesario pasar nada pa instanciar la clase 
         Neuron.__init__(self)
+        #la construccion de la calse base debe ser ejecutada para configurar todas las propiedades aqui
+        #las propiedades mas importantes de entrada y valor
         # el nodo de entrada es el unico donde el valor puede ser pasado como un argumento 
         #para la funcion forward, todos los demas nodos deben obtener el valor del nodo anterior
         #de self.nodos_entrada
+        #self.valor es definido en la función topologia_sort()
         
     def forward(self):
+        #nada se hace en esta etapa
         pass
     
     def backward(self):
         #un nodo de entrada no posee inputs de este modo el gradiente o derivada es cero
+        
         self.gradientes = {self:0}
         
         #Los pesos y bias pueden ser entradas, entonces es necesario sumar el gradiente, de los gradientes de salida
@@ -197,7 +204,7 @@ def forward_backward(graph):
     """
     #forward pass
     for n in graph:
-        print(n.valor)
+        #print(n.valor)
         n.forward()
         
     # backward pass
@@ -206,8 +213,22 @@ def forward_backward(graph):
         n.backward()
     
 
+def sgd_update(params, learning_rate = 1e-2):
+    """
+    Actualiza el valor de cada parametro entrenable com el SGD
+    learning_rate : tasa de aprendizaje.
+    """
+    
+    #ejecuciñon deSGD
+    
+    #Loop sobre todos los parametros
+    for t in params:
+        # alterar el valor del parametro sustrayendo la tasa de aprendizaje multiplicando por la parte del coste en relación a ese parametro
+        partial = t.gradientes[t]
+        t.valor -= learning_rate*partial
 
-
+"""
+#### Teste con muestras aleatorias ··
 #ejecución del grafo
     
 # definimos los inputs (todo esto es solo para inicializar nodos aqui nada tiene valor)
@@ -239,5 +260,90 @@ forward_backward(graph)
 # retorna los gradientes de cada input
 gradientes = [t.gradientes[t] for t in [X, y, W, b]]
 
-
 print(gradientes)
+"""
+
+
+#Teste con un dataset real para predecir preciosa de casas
+
+from sklearn.datasets import load_boston
+from sklearn.utils import shuffle,resample
+
+#cargar los datos
+data = load_boston()
+
+# divir variables de entrada y salida
+X_ = data['data']
+y_ = data['target']
+
+#normalizar los datos
+X_ =(X_ -np.mean(X_,axis = 0 )) / np.std(X_, axis=0)
+
+#Numero de caracteristicas e neuronas
+n_features = X_.shape[1]
+n_hidden = 10
+
+#Definiendo valores aleatorios para inicializar pesos y bias
+W1_ = np.random.randn(n_features, n_hidden)
+b1_ = np.zeros(n_hidden)
+W2_ = np.random.randn(n_hidden,1)
+b2_ =np.zeros(1)
+
+#Arquitectura Red GRAPH
+X,y = Input(), Input()
+W1,b1 = Input(), Input()
+W2,b2 = Input(), Input()
+
+l1 = Lineal(X,W1,b1)
+s1 = Sigmoid(l1)
+l2 = Lineal(s1,W2,b2)
+cost= CostFunction(y,l2)
+
+# definimos el feed_dict
+
+feed_dict = {X:X_,
+             y:y_,
+             W1:W1_,
+             b1:b1_,
+             W2:W2_,
+             b2:b2_
+             }
+
+#numero de epocas
+epochs = 1000
+
+#numero total de registros
+m = X_.shape[0]
+print("El numeor total de registros es: {}".format(m))
+
+#tamaño de cada lote
+batch_size = 11
+steps_per_epoch = m // batch_size
+
+#Definimos el grafo computacinoal
+graph = topologia_sort(feed_dict)
+
+#valores que seran aprendidos por la red
+params = [W1,b1,W2,b2]
+
+#Entrenamiento del modelo
+for i in range(epochs):
+    loss = 0
+    for j in range(steps_per_epoch):
+        
+        #paso 1 -- testa aleatoriamente un lote de registros
+        X_batch, y_batch = resample(X_,y_,n_samples = batch_size)
+        
+        # reset de los valores de Xy y
+        X.valor = X_batch
+        y.valor = y_batch
+        
+        #paso2 -- forward y backpropagation
+        forward_backward(graph)
+        
+        #paso 3 -- optimizacion por SGD
+        sgd_update(params)
+        
+        loss += graph[-1].valor
+        
+    print("Epcoa {}, error: {:.3f}".format(i+1, loss/steps_per_epoch))
